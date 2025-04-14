721f844bae6cc7edf6352680a3f9c2685533fbd2
julien-blit <julien.ravier@outlook.fr>
2025-04-14 16:56:59 +0200

    ajout de nouveau fichiers

---
+++ script.js
@@ -0,0 +1,17 @@
+ // ! Mode Darck //
+
+const toggleBtn = document.getElementById('dark-mode-toggle');
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    // Change l’emoji selon le thème
+    toggleBtn.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+  });
+
+
+//! navbar //
+  const menuBtn = document.getElementById('btn');
+const liens = document.getElementById('nav-links');
+
+menuBtn.addEventListener('click', function() {
+    liens.classList.toggle('show');
+});
